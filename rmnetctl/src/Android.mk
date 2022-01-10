1fa577758e26aec82a17d154e446c0c662015369
Alexander Martinz <alex@amartinz.at>
2022-01-10 10:18:12 +0100

    BRINGUP
    Change-Id: I115b68bc28ebba6487c1e7bc1bcee0ea0d4e949d
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- rmnetctl/src/Android.mk
+++ rmnetctl/src/Android.mk
@@ -1,9 +1,9 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
+#LOCAL_PATH := $(call my-dir)
+#include $(CLEAR_VARS)
 
-LOCAL_VENDOR_MODULE := true
+#LOCAL_VENDOR_MODULE := true
 
-LOCAL_COPY_HEADERS_TO   := dataservices/rmnetctl
-LOCAL_COPY_HEADERS      := ../inc/librmnetctl.h
+#LOCAL_COPY_HEADERS_TO   := dataservices/rmnetctl
+#LOCAL_COPY_HEADERS      := ../inc/librmnetctl.h
 
-include $(BUILD_COPY_HEADERS)
+#include $(BUILD_COPY_HEADERS)
