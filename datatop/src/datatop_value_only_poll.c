651d2a1182bf440034a4ef901bfd15f1fb4f14a4
наб <nabijaczleweli@nabijaczleweli.xyz>
2025-05-25 22:52:41 +0200

    Fix dtop_value_only_search() name formatting
    Found by:
      datatop_value_only_poll.c:176:4: warning: 'snprintf' will always be truncated; specified size is 4, but format string expands to at least 5 [-Wformat-truncation]
        176 |                         snprintf(newname, sizeof(10), "[%d]:", i);
            |                         ^
      1 warning generated.
    
    Change-Id: I56fca0df473a42c0a1af1ec0fc9fb9e3ae9fa624

--- datatop/src/datatop_value_only_poll.c
+++ datatop/src/datatop_value_only_poll.c
@@ -166,18 +166,14 @@ int dtop_value_only_search(char *name)
 
 	/* Creates a dtop_data_point struct for each dp found in the file */
 	for (i = 0; i < dict.max; i++) {
-		char *newname = malloc(sizeof(10));
 		if (dict.val[i][0] == '-')
 			data_points[i].type = DTOP_LONG;
 		else
 			data_points[i].type = DTOP_ULONG;
-		data_points[i].name = malloc(sizeof(10));
 		if (dict.max > 1)
-			snprintf(newname, sizeof(10), "[%d]:", i);
+			asprintf(&data_points[i].name, "[%d]:", i);
 		else
-			strlcpy(newname, "", sizeof(10));
-		strlcpy(data_points[i].name, newname, sizeof(10));
-		free(newname);
+			data_points[i].name = strdup("");
 		data_points[i].prefix = NULL;
 		data_points[i].skip = DO_NOT_SKIP;
 		data_points[i].initial_data_populated = NOT_POPULATED;
